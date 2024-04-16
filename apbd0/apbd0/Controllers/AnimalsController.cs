b58dccec4492d0a5417e569100b86419494cee21
Patrycja <s28518@pjwstk.edu.pl>
2024-04-16 13:17:33 +0200

    Add class AnimalsController

---
+++ apbd0/apbd0/Controllers/AnimalsController.cs
@@ -0,0 +1,52 @@
+﻿using apbd0.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace apbd0.Controllers;
+
+[ApiController]
+// [Route("api/animals")]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        // Uruchamiamy połączenie do bazy
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        //Definiujemy command
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM Animal";
+        
+        // Uruchomienie zapytania
+        var reader = command.ExecuteReader();
+
+        List<Animal> animals = new List<Animal>();
+
+        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        int nameOrdinal = reader.GetOrdinal("Name");
+        
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                // IdAnimal = reader["IdAnimal"].ToString(),
+                // Name = reader["Name"].ToString()
+                
+                IdAnimal = reader.GetInt32(idAnimalOrdinal),
+                Name = reader.GetString(nameOrdinal)
+            });
+        }
+        
+        return Ok(animals);
+    }
+}
